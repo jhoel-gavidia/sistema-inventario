71b101f5d33853399697c37c3c4c93a38163461e
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 10:45:18 -0500

    feat: crear rutas endpoint Proveedor

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProveedorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+//RUTAS CATEGORIA
 
 Route::get('/categorias', [CategoriaController::class, 'index']);
 
@@ -11,3 +14,13 @@ Route::post('/categorias', [CategoriaController::class, 'store']);
 Route::put('/categorias/{id}', [CategoriaController::class, 'update']);
 
 Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy']);
+
+//RUTAS PROVEEDOR
+
+Route::get('/proveedores', [ProveedorController::class, 'index']);
+
+Route::post('/proveedores', [ProveedorController::class, 'store']);
+
+Route::put('/proveedores/{id}', [ProveedorController::class, 'update']);
+
+Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy']);

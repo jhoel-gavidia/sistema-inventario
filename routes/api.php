7a41870b3fedc8e6d0eb50436ab8b0fcc11f6c63
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 10:04:04 -0500

    feat: crear tura delete endpoint destroy

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,5 @@ Route::get('/categorias', [CategoriaController::class, 'index']);
 Route::post('/categorias', [CategoriaController::class, 'store']);
 
 Route::put('/categorias/{id}', [CategoriaController::class, 'update']);
+
+Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy']);

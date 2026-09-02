ca9787768a5eb92b670636da663400807d0ec838
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 11:35:03 -0500

    feat: implementar rutas endpoint Productos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ProveedorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,13 @@ Route::post('/proveedores', [ProveedorController::class, 'store']);
 Route::put('/proveedores/{id}', [ProveedorController::class, 'update']);
 
 Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy']);
+
+// RUTAS PRODUCTO
+
+Route::get('/productos', [ProductoController::class, 'index']);
+
+Route::post('/productos', [ProductoController::class, 'store']);
+
+Route::put('/productos/{id}', [ProductoController::class, 'update']);
+
+Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);

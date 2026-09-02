161b7ed980d3ff0a853b8688d7a08e35ef764431
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 09:22:06 -0500

    feat: crear ruta para endpoint index

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/categorias', [CategoriaController::class, 'index']);

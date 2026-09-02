e8061910aacab1824a66613247340fd3c0771cff
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 09:07:04 -0500

    feat: crear migración movimientos stock

---
+++ database/migrations/2026_09_02_134640_create_movimientos_stock_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movimientos_stock', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producto_id')->constrained('productos');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('tipo', ['entrada', 'salida']);
+            $table->unsignedInteger('cantidad');
+            $table->string('motivo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movimientos_stock');
+    }
+};

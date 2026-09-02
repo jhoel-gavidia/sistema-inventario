531e3eb96902c5a1b917d1c4a736bb8087952392
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 09:53:45 -0500

    feat: crear ruta put endpoint update

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/categorias', [CategoriaController::class, 'index']);
+
+Route::post('/categorias', [CategoriaController::class, 'store']);
+
+Route::put('/categorias/{id}', [CategoriaController::class, 'update']);

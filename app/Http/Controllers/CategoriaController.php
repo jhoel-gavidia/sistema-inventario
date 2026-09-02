c4191717d0bfaae90e7b1deb1e08aa342eef846b
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-02 09:31:45 -0500

    feat: implementar metodo store en CategoriaController

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -7,9 +7,34 @@ use Illuminate\Support\Facades\DB;
 
 class CategoriaController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $select = DB::select('select * from categorias');
 
         return response()->json($select);
+    }
+
+    public function store(Request $request)
+    {
+        $nombre = $request->input('nombre');
+        $descripcion = $request->input('descripcion');
+
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+        ]);
+
+        DB::insert(
+            "INSERT INTO categorias (nombre, descripcion, created_at, updated_at) VALUES(?,?,?,?)",
+            [$nombre, $descripcion, now(), now()]
+        );
+
+        $id = DB::getPdo()->lastInsertId();
+
+        return response()->json([
+            'id'=>$id,
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+        ], 201);
     }
 }
